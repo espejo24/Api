3c9768c0365a768a0f2f3d8ac5df96baff434265
Maria Markina <rbsk@bk.ru>
2022-02-03 13:34:33 +0300

    Inicial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    public String name;                                   // поле для имени
+    public String passport;                              // добавьте другие поля
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+
+
+    }
+
+
